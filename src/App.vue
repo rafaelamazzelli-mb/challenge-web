01a4529b19228dfdc704ba39784a9fad0b6d4183
Rafaela Mazzelli <rafaela.mazzelli@mb.com.br>
2025-08-28 15:22:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Review from './components/Review.vue'
+</script>
+
+<template>
+  <Review />
+</template>
+
+<style scoped></style>
---
+++ src/components/Review.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <div class="title-review-your-informations">
+    <h3>Revise suas informações</h3>
+  </div>
+
+  <div class="form-container">
+    <form action="">
+      <label for="e-mail"> Endereço de e-mail</label>
+      <input type="email" name="email-address" />
+
+      <label for="name">Nome</label>
+      <input type="text" name="nome" />
+
+      <label for="cpf">CPF</label>
+      <input type="text" name="cpf" />
+
+      <label for="date">Data de nascimento</label>
+      <input type="text" name="date" />
+
+      <label for="telephone">Telefone</label>
+      <input type="text" name="telephone" />
+
+      <label for="password">Senha</label>
+      <input type="text" name="password" />
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
