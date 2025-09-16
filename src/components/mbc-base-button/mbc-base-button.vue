7ff3a2f418612cfdf067c4d4db406d19d40003b5
Rafaela Mazzelli <rafaela.mazzelli@mb.com.br>
2025-09-16 10:40:20 -0300

    perf: feito os ajustes no componente base-button

--- src/components/mbc-base-button/mbc-base-button.vue
+++ src/components/mbc-base-button/mbc-base-button.vue
@@ -1,27 +1,18 @@
 <template>
-  <button :class="buttonVariantType">{{ props.label }}</button>
+  <button class="button" :class="props.variant">{{ props.label }}</button>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
-
 const props = defineProps({
-  type: {
-    type: String,
-  },
   label: {
     type: String,
   },
-})
-
-const enabled = ref(true)
-
-const buttonVariantType = computed(() => {
-  return {
-    button: enabled,
-    primary: props.label === 'Continuar' || 'Cadastrar' ? enabled : !enabled,
-    secondary: props.label === 'Voltar' ? enabled : !enabled,
-  }
+  variant: {
+    type: String,
+    validator(value) {
+      return ['primary', 'secondary'].includes(value)
+    },
+  },
 })
 </script>
 
